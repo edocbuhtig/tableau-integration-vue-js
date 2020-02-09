7e15fd8e83cf94268e04ca086b544d1cacb270e9
Sunil Kumar <45172271+edocbuhtig@users.noreply.github.com>
2020-02-09 14:21:07 +0530

    Add files via upload

---
+++ src/components/TableauHolder.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="vizContainer">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableauHolder',
+  methods: {
+        initViz () {
+            const containerDiv = document.getElementById("vizContainer")
+            
+            let url = "http://public.tableau.com/views/RegionalSampleWorkbook/Storms"
+
+            let options = {
+                hideTabs: true,
+                onFirstInteractive: () => { 
+
+                }
+            }
+            
+            this.viz = new window.tableau.Viz(containerDiv, url, options)
+        }
+    },
+    mounted () {
+        window.addEventListener('load', () => {
+            this.initViz()
+        })
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
